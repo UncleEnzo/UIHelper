325f9b7544927949a6fb1773f52f5553782220c4
Nick Evelson <nevelson92@gmail.com>
2022-10-28 13:55:59 -0400

    Added refresh to selectables for dynamically generated UI

--- Runtime/Scripts/Abstracts/UIScreenBase.cs
+++ Runtime/Scripts/Abstracts/UIScreenBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -113,22 +114,87 @@ namespace Nevelson.UIHelper
         public void LockSelectables()
         {
             InitSelectables();
+
+            bool failure = false;
             foreach (KeyValuePair<Selectable, Navigation> selectable in selectables)
             {
                 //Debug.Log($"Locking UI element: {selectable.Key.gameObject.name}");
+                if (selectable.Key == null)
+                {
+                    failure = true;
+                    break;
+                }
                 selectable.Key.interactable = false;
                 selectable.Key.navigation = navigationNone;
+            }
+
+            if (failure)
+            {
+                UpdateSelectables();
+                LockSelectables();
             }
         }
 
         public void UnlockSelectables()
         {
             InitSelectables();
+
+            bool failure = false;
             foreach (KeyValuePair<Selectable, Navigation> selectable in selectables)
             {
                 //Debug.Log($"Unlocking UI element: {selectable.Key.gameObject.name}");
+                if (selectable.Key == null)
+                {
+                    failure = true;
+                    break;
+                }
                 selectable.Key.interactable = true;
                 selectable.Key.navigation = selectable.Value;
+            }
+
+            if (failure)
+            {
+                UpdateSelectables();
+                UnlockSelectables();
+            }
+        }
+
+        void InitSelectables()
+        {
+            if (selectables != null)
+            {
+                return;
+            }
+
+            selectables = new Dictionary<Selectable, Navigation>();
+            Selectable[] selectableUIElements = GetComponentsInChildren<Selectable>(true);
+            foreach (var selectable in selectableUIElements)
+            {
+                selectables.Add(selectable, selectable.navigation);
+            }
+        }
+
+        void UpdateSelectables()
+        {
+            Debug.Log("Regenerating selectables dictionary");
+
+            //Clever Unity specific work around to deleting nulls in unity only
+            foreach (var key in selectables.Keys.ToArray())
+            {
+                if (key == null)
+                {
+                    selectables.Remove(key);
+                }
+            }
+
+            Selectable[] selectableUIElements = GetComponentsInChildren<Selectable>(true);
+            foreach (var selectable in selectableUIElements)
+            {
+                if (!selectables.ContainsKey(selectable))
+                {
+                    Debug.Log($"Adding new selectable: {selectable.gameObject.name}");
+                    selectables.Add(selectable, selectable.navigation);
+                }
             }
         }
 
@@ -193,20 +259,6 @@ namespace Nevelson.UIHelper
         }
 
         protected virtual void Update() { }
-
-        void InitSelectables()
-        {
-            if (selectables != null)
-            {
-                return;
-            }
-            selectables = new Dictionary<Selectable, Navigation>();
-            Selectable[] selectableUIElements = GetComponentsInChildren<Selectable>(true);
-            foreach (var selectable in selectableUIElements)
-            {
-                selectables.Add(selectable, selectable.navigation);
-            }
-        }
 
         void ResetUIManagers()
         {
